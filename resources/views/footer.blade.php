82c0624b0e7606aca7f2ad67907babb57be6a3d5
sthachendev <sthachendev@gmail.com>
2023-12-23 18:16:23 +0600

    +footer

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- bootstrap -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+  <!-- font awesome icons -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+   <!-- Include Poppins font from Google Fonts -->
+   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+  <style>
+    body {
+      min-height: 100vh;
+      display: flex;
+      flex-direction: column;
+      font-family: 'Poppins', sans-serif;
+    }
+
+    main {
+      flex: 1;
+    }
+  </style>
+</head>
+<body>
+
+  <main>
+    <!-- Your main content goes here -->
+  </main>
+
+  <footer class="text-light py-4" style="background:#24408F;">
+    <div class="container">
+      <div class="row p-3">
+        <!-- Left side with text logo and social media icons -->
+        <div class="col-md-6 text-md-start mb-3 mb-md-0">
+          <span class="fw-bold fs-4 text-light">
+            BAYUEL KINZANG YOESEL 
+            <br>
+            CHOLING FOUNDATION
+            </span>
+          <div class="mt-2 d-flex">
+            <!-- Add your social media icons here -->
+            <a href="https://www.facebook.com/YourFacebookPage" class="text-light me-3 d-flex align-items-center justify-content-center text-decoration-none"
+            style="background: #4F5B9E; width: 30px; height: 30px; border-radius: 50%;" target="_blank" rel="noopener noreferrer">
+                <i class="fab fa-facebook"></i>
+            </a>
+
+            <a href="https://www.instagram.com/YourInstagramUsername" class="text-light me-3 d-flex align-items-center justify-content-center text-decoration-none"
+            style="background: #4F5B9E; width: 30px; height: 30px; border-radius: 50%;" target="_blank" rel="noopener noreferrer">
+                <i class="fab fa-instagram"></i>
+            </a>
+
+            <a href="https://twitter.com/YourTwitterHandle" class="text-light me-3 d-flex align-items-center justify-content-center text-decoration-none"
+            style="background: #4F5B9E; width: 30px; height: 30px; border-radius: 50%;" target="_blank" rel="noopener noreferrer">
+                <i class="fab fa-twitter"></i>
+            </a>
+
+            <a href="https://t.me/YourTelegramUsername" class="text-light me-3 d-flex align-items-center justify-content-center text-decoration-none"
+            style="background: #4F5B9E; width: 30px; height: 30px; border-radius: 50%;" target="_blank" rel="noopener noreferrer">
+                <i class="fab fa-telegram-plane"></i>
+            </a>
+
+            <a href="https://wa.me/1234567890" class="text-light me-3 d-flex align-items-center justify-content-center text-decoration-none"
+            style="background: #4F5B9E; width: 30px; height: 30px; border-radius: 50%;" target="_blank" rel="noopener noreferrer">
+                <i class="fab fa-whatsapp"></i>
+            </a>
+
+          </div>
+        </div>
+        
+        <!-- Right side with quick links -->
+        <div class="col-md-6 text-md-end" style="background:#4F5B9E">
+        <div class="d-flex justify-content-around p-3">
+
+        <div class="d-flex flex-column p-3 text-start">
+          <h6 class="text-light mb-3">Get Involved</h6>
+          <a href="#" class="text-light text-decoration-none mb-2" style="font-size:12px">Center</a>
+          <a href="#" class="text-light text-decoration-none mb-2" style="font-size:12px">Activities</a>
+          <a href="#" class="text-light text-decoration-none mb-2" style="font-size:12px">About Us</a>
+          <a href="#" class="text-light text-decoration-none mb-2" style="font-size:12px">Support Us</a>
+          <a href="#" class="text-light text-decoration-none mb-2" style="font-size:12px">Fundraising</a>
+        </div>
+
+        <div class="d-flex flex-column p-3 text-start">
+          <h6 class="text-light mb-3">Contact Us</h6>
+          <span class="text-light text-decoration-none mb-2" style="font-size:12px">Olakha</span>
+          <span class="text-light text-decoration-none" style="font-size:12px">Thimphu, Bhutan</span>
+          <br>
+          <span class="text-light text-decoration-none mb-2" style="font-size:12px">Email: info@peldrukdraling.org</span>
+          <span class="text-light text-decoration-none" style="font-size:12px">Phone: +975-17618421</span>
+        </div>
+        
+          </div>
+        </div>
+      </div>
+      
+      <hr>
+      <div class="text-center" style="font-size:12px">BAYUEL KINZANG YOESEL CHOLING FOUNDATION &copy 2023</div>
+    </div>
+  </footer>
+
+  <!-- Bootstrap 5 JS and Popper.js -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>

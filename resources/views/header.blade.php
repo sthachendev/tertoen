340fedfd8588f4c4af3986a7ff49560394297328
sthachendev <sthachendev@gmail.com>
2024-01-20 15:31:04 +0600

    Update header.blade.php
    Rectify the navbar that is unable to open while collapse

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -128,7 +128,7 @@
 
     </header>
 
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>

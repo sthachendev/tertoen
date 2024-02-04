4305b066b0563b3395f0a29712c5ce2d05eaa60e
sthachendev <sthachendev@gmail.com>
2024-02-04 20:50:38 +0600

    Update about_us.blade.php
    updated the content

--- resources/views/about_us.blade.php
+++ resources/views/about_us.blade.php
@@ -9,7 +9,13 @@
         p {
             font-size: 14px;
             text-align: justify;
-            line-height: 1.5;
+            line-height: 29px;
+            margin: 0 10% 0 10%;
+        }
+
+        li {
+            line-height: 29px;
+            font-size: 14px;
         }
     </style>
 </head>
@@ -25,45 +31,214 @@
     <h4 id="title" class="text-center my-3" data-aos="fade-right">
         BAYUEL KINZANG YOESEL CHOLING FOUNDATION
     </h4>
-
-    <p class="text-secondary" data-aos="fade-right" style="margin:0 10% 0 10%; line-height: 29px;">
-        The reincarnation of Terton Drukdra Dorji Rinpoche, known as His Eminence Ugyen Droduel Thinley Kunchap Rinpoche
-        has established the Pel Drukdraiing Foundation with the support from his numerous followers, donors, and
-        patrons. The Foundation name, Drukdra, is after the country of Bhutan known as Druk and Terton Drukdra Dorji.
-        The activities of the Pel Drukdraling Foundation are guided by compassion and wisdom. The Foundation seeks to
-        promote and strengthen human values with a sense of spirituality that fosters universal peace and harmony.
+    <br>
+    <p class="text-secondary" data-aos="fade-right" style="margin:0 10% 0 10%;">
+        The name of this Foundation is "Baeyuel Kuenzang Woesel Choeling Foundation " and is a Religious Organization, a
+        non-profit organization established to develop as a Spiritual and Cultural Centre in order to preserve, promote
+        and propagate Buddha Dharma (Vajrayana) teachings of Tertoen Drukdra Dorji Yangsi, His Holiness Trulku Ugyen
+        Drodrul Thinley Kunchap, who was formally recognised and enthroned by the present 70th Je Khenpo Jigme Chodra
+        and the Central Monastic Body of Bhutan.
         <br>
         <br>
-        The Foundation endeavours to preserve and promote the culture and heritage of the country of Bhutan and improve
-        social and community vitality through education. Moreover, the Foundation will establish and support a needed
-        individuals in and around the country for their education, health and wellbeing. The foundation aims to engage
-        in projects that will provide basic needs, housing, medical care, and education for monks. Similarly, the
-        foundation shall endeavour to support children from poor and disadvantaged backgrounds and the elderly citizens
-        both in Bhutan and wider world in the foreseeable future.
+        The Organization under the Presidential of His Holiness Trulku Ugyen Drodrul Thinley Kunchap shall function to
+        create peace, prosperity and wellbeing of rural communities of least developed and unite all religious
+        individuals aspiring to take professionalism in promoting the spiritual welfare of sentient beings in the
+        country and contribute the growth of Buddhist teaching and Buddha Dharma that will ultimately benefit our
+        religious institutions and spiritual heritage of Bhutan.
         <br>
         <br>
-        As able, the Foundation will take on other beneficial and social activities. The Foundation is governed by a
-        board of volunteer members that follow the guidance of Rinpoche who is the Spriritual Head, President and
-        Founder of Pel Drukdraling Foundation.
+        <strong>Baeyuel Kuenzang Woesel Choeling Foundation (BKWC Foundation)</strong>
+        was registered on 17th November, 2020 under the
+        Commission for Religious Organizations.
     </p>
 
     <br>
     <h4 id="title" class="text-center my-3" data-aos="fade-right">
-        OBJECTIVIES
+        Office and Duration
     </h4>
 
-    <p style="margin:0 10% 0 10%" class="text-secondary" data-aos="fade-right">
-        1. To provide spiritual counselling and support to the recovening adicts and children from disadvantagedd
-        backgrounds
+    <p style="margin:0 10% 0 10%; line-height: 29px;" class="text-secondary" data-aos="fade-right">
+        The office of the Foundation shall be located at:
+        <br>Flat No. 32, Building No. 5,
+        <br>Changzamtog, Thim Throm,
+        <br>Thimphu 11001, Bhutan.
+        <br>Or at such place as the Foundation may from time to time determine, or as the business of the Foundation may
+        require.
+        <br><br>
+        Geographical Area of Operation:
         <br>
-        <br>
-        2. To predserve and promote aged-old culture and tradition of Bhutan, and
-        <br>
-        <br>
-        3. To support and help elderly citizens in Bhutan and wider world in the foreseeable future.
+        Bongo Geog, Chukha Dzongkhag
+        <br><br>
+        The duration of the Foundation shall be perpetual. The death, removal, or resignation of any member of the
+        Foundation shall not result in the dissolution of the Foundation.
     </p>
 
     <br>
+    <h4 id="title" class="text-center my-3" data-aos="fade-right">
+        Purpose
+    </h4>
+
+    <p style="margin:0 10% 0 10%; line-height: 29px;" class="text-secondary" data-aos="fade-right">
+        <strong>Baeyuel Kuenzang Woesel Choeling Foundation (BKWC Foundation)</strong> a non-profit Religious
+        Organization,
+        established for the benefit of religious institutions and spiritual heritage of Bhutan. The Foundation has been
+        established to unite all religious individuals aspiring to take professionalism in promoting the welfare of all
+        sentient beings for spiritual and religious development.
+    </p>
+    <br>
+    <p style="margin:0 10% 0 10%; line-height: 29px;" class="text-secondary" data-aos="fade-right">
+        The purpose and functions of BKWC Foundation are:
+        <br>
+    <ul style="margin:0 10% 0 10%;" style="list-style-type: square;" class="text-secondary" data-aos="fade-right">
+        <li>Protect and promote the constitutional right to information, freedom of expression and religions.
+        </li>
+        <li>Create charity, avenue and platforms to strengthen the living conditions, employment and livelihood of poor
+            and elderly people of communities.
+        </li>
+        <li>Foster a growing number of professionally trained religious individuals for the benefit of common rural
+            people.
+        </li>
+        <li>
+            Protect common people from hazards such as threats, harassments, litigation, etc. from the external
+            influences and manipulations.
+        </li>
+        <li>
+            Support people who are passionate about becoming a religious person.
+        </li>
+        <li>
+            Promote interaction and exchange of ideas for the professional development of its members.
+        </li>
+        <li>
+            Explore funding sources to collaborate and invest in professional programs for spiritual and religious
+            development.
+        </li>
+        <li>
+            Institute mechanism to recognise a religious person through awards and grants.
+        </li>
+        <li>
+            Stimulate and sustain professional debate on crucial issues through seminars, workshops and discussion
+            forum.
+        </li>
+        <li>
+            Maintain and promote a high standard of ethical behaviour in the practice of <strong>BKWC
+                Foundation.</strong>
+        </li>
+    </ul>
+    </p>
+
+    <br>
+    <h4 id="title" class="text-center my-3" data-aos="fade-right">
+        Objectives
+    </h4>
+
+    <p style="margin:0 10% 0 10%; line-height: 29px;" class="text-secondary" data-aos="fade-right">
+        The objectives of the Foundation are to serve as a common platform & a national point of reference for all the
+        Foundation members in Bhutan and to serve as a forum and point of contact on policy matters, legislation, and
+        regulations affecting the religious organization and its activities & to interface with government agencies and
+        other authorities on issues concerning the religious organization and it's promotions. The objectives of the
+        Foundation are, inter alia, to:
+    <ul style="margin:0 10% 0 10%;" style="list-style-type: square;" class="text-secondary" data-aos="fade-right">
+        <li>Establish & promote code of ethics, formulate strategies and legislation to accelerate the promotion of
+            religious organization and its activities in Bhutan;
+        </li>
+        <li>
+            Foster friendship and promote networking of Foundation members;
+        </li>
+        <li>Facilitate and promote cooperation among religious and non-religious members and other stakeholders within
+            country and develop global cooperation and establish interaction links with neighbouring countries and
+            international level be at an individual, group or institutional level;
+        </li>
+        <li>
+            Promote exchanging ideas, knowledge and information amongst Foundation members and participate to share with
+            regional neighbouring and global countries;
+        </li>
+        <li>Support common people engagement and communities of religious practice and its development for the benefit
+            and wellbeing of the society;
+        </li>
+        <li>Cultivate leadership, support and promote public diplomacy;
+        </li>
+        <li>Mediate & arbitrate (resolve), represent, foster, develop, encourage and improve the religious organization.
+        </li>
+        <li>Promote and support regional fund raising activities with other Religious Organization, NGO and Foundations
+            to contribute social benefits.
+        </li>
+    </ul>
+    </p>
+
+    <br>
+    <h4 id="title" class="text-center my-3" data-aos="fade-right">
+        Scope
+    </h4>
+
+    <p style="margin:0 10% 0 10%; line-height: 29px;" class="text-secondary" data-aos="fade-right">
+        The Foundation mandates to create a database of Foundation members with relevant information about their
+        contact, education background and work experiences so that relevant information pertaining to the development
+        and contribution towards the preservations and promotion of religious organization and its activities in Bhutan
+        are maintained and any other matters of interest could be timely disseminated accordingly. The scope of
+        activities of the Foundation shall comprise any activities that contribute to the achievement of the
+        Foundation's purpose and objectives, and may include, without limitation:
+    <ul style="margin:0 10% 0 10%;" style="list-style-type: square;" class="text-secondary" data-aos="fade-right">
+        <li>Represent Foundation members both within and outside the country.
+        </li>
+        <li>Integration activities for all Foundation members;
+        </li>
+        <li>Revolutionize the profession of Foundation members to earn respect, support and admiration from the
+            communities;
+        </li>
+        <li>Mentoring activities and counselling of young aspiring and volunteers for the participation in the religious
+            organization and its development;
+        </li>
+        <li>Support and develop religious arts and related skills to promote talents and preserve Bhutan's unique
+            traditional and cultural heritage. Take necessary steps to promote, encourage and assist in the development
+            of the religious artists in Bhutan;
+        </li>
+        <li>Promotional and social activities, organise workshops, trainings and seminars;
+        </li>
+        <li>Facilitate services and resources to the members of BKWC Foundation to achieve the common goal and
+            objectives of religious activities and it's promotions;
+        </li>
+        <li>To communicate and act as a liaison with the Government, national and international organizations,
+            Foundations and other mercantile to promote measures in the interest of the religious members;
+        </li>
+        <li>Institute a regional administration offices or a religious stadium as deemed required for all aspiring
+            Foundation and religious members to learn, educate religions, develop skills and performance that will
+            attain an individual member's career sustainability and the source of income;
+        </li>
+        <li>Participation in the regional countries and international religious markets;
+        </li>
+        <li>Participate in the international markets through the adaptation and diversification of religion innovations
+            and products. Obtain affiliation with similar organizations in other countries.
+        </li>
+        <li>Promote our Buddhist religions among youth, facilitate the development of the religious art into a
+            profitable and dignified vocation and career in order to contribute enhancing the social livelihood and a
+            source of income;</li>
+        <li>Establish platform for collaboration amongst those aspiring youth talents, innovations and showcase the
+            religious art and literature.
+        </li>
+        <li>Institute awards for excellence in Buddhist art and literature and related activities. Organize and promote
+            members award and event functions to celebrate/recognize members talent and achievements;
+        </li>
+        <li>Organize exhibitions, fund raising activities and religious fairs to promote our culture, skills,
+            performance and the potential of the Foundation members to generate fund and income;
+        </li>
+        <li>Provide avenue, market and receive religious event contracts from the Government, corporate and private
+            organizations for the members of the Foundation (Source of income for Foundation members);
+        </li>
+        <li>Organise and manage the activities of Foundation members for religious contract and event hosting;
+        </li>
+        <li>Initiate programs that develop and promote the educational, cultural, and vocational relationship with
+            neighbouring countries;
+        </li>
+        <li>Promoting all lawful activities that may be incidental or reasonably necessary to achieve the purposes of
+            the Foundation.
+        </li>
+    </ul>
+    </p>
+    <p style="margin:0 10% 0 10%; line-height: 29px;" class="text-secondary" data-aos="fade-right">
+        The scope of BKWC Foundation envisions for the highest degree of professionalism amongst members with Code of
+        Conduct that all of its members must adhere to, besides conducting several trainings and workshops to hone and
+        meliorate the quality of religious organization.
+    </p>
     <h4 id="title" class="text-center my-3" data-aos="fade-right">
         BOARD OF DIRECTORS
     </h4>

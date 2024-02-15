56e4524d068de174332d18a5d3056008977b67f9
Kelden <kelyang509@gmail.com>
2024-02-16 01:48:14 +0600

    Home page UI changes

--- resources/views/home-content.blade.php
+++ resources/views/home-content.blade.php
@@ -10,27 +10,72 @@
         }
     </style>
 </head>
-<img src="{{ asset('images/rinpoche-image-1.png') }}" alt="logo" class="mx-5" data-aos="fade-right"
-    data-aos-easing="ease" data-aos-duration="1000">
 
-<h4 id="title" class="text-center mt-3" data-aos="fade-right">
-    H.H Ugyen Droduel Thinley Kunchap Rinpoche
-</h4>
 
-<div>
-</div>
-<main class="p-3 px-5" data-aos="fade-right">
-    <p style="font-size:14px; line-height: 29px;" class="text-secondary">
-        The reincarnation of Terton Drukdra Dorji Rinpoche, known as His Eminence Ugyen Droduel Thinley
-        Kunchap Rinpoche has established the Pel Drukdraiing Foundation with the support from his numerous
-        followers, donors, and patrons. The Foundation name, Drukdra, is after the country of Bhutan known
-        as Druk and Terton Drukdra Dorji. The activities of the Pel
-        Drukdraling Foundation are guided by compassion and wisdom. The Foundation seeks to promote and strengthen.
-    </p>
-</main>
 
-<div style="display: flex; justify-content:center;" class="mt-0 mb-5">
-    <iframe width="60%" height="340" src="https://www.youtube.com/embed/s4pxZUCmHsc" title="Gorina" frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        allowfullscreen></iframe>
+
+
+<div class="container text-center ">
+  <div class="row my-4">
+    <!-- Main content -->
+    <div class="col-12 col-md-8 col-lg-8">
+
+    <img src="{{ asset('images/rinpoche-image-1.png') }}" width="630"  alt="logo" class="img-fluid"
+        data-aos="fade-right" data-aos-easing="ease" data-aos-duration="1000">
+
+            <h4 id="title" class="text-center mt-3" data-aos="fade-right">
+            H.H Ugyen Droduel Thinley Kunchap Rinpoche
+        </h4>
+        <div>
+        </div>
+        <main class="p-3 px-5" data-aos="fade-right">
+            <p style="font-size:14px; line-height: 29px;" class="text-secondary">
+                The reincarnation of Terton Drukdra Dorji Rinpoche, known as His Eminence Ugyen Droduel Thinley
+                Kunchap Rinpoche has established the Pel Drukdraiing Foundation with the support from his numerous
+                followers, donors, and patrons. The Foundation name, Drukdra, is after the country of Bhutan known
+                as Druk and Terton Drukdra Dorji. The activities of the Pel
+                Drukdraling Foundation are guided by compassion and wisdom. The Foundation seeks to promote and strengthen.
+            </p>
+        </main>
+
+        <div style="display: flex; justify-content:center;" class="mt-0 mb-5">
+            <iframe width="60%" height="340" src="https://www.youtube.com/embed/s4pxZUCmHsc" title="Gorina" frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                allowfullscreen></iframe>
+        </div>
+    </div>
+    <!-- Side Content -->
+    <div class="col-12 col-md-4 col-lg-4">
+
+
+
+        <div class="input-group p-3"  data-aos="fade-right">
+            <input type="text" class="form-control" placeholder="Search" aria-label="Search" aria-describedby="button-addon2">
+            <button class="btn btn-outline-secondary" type="button" id="button-addon2">
+                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-search" width="24"
+                    height="24" viewBox="0 0 24 24" stroke-width="1" stroke="gray" fill="none"
+                    stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
+                    <path d="M21 21l-6 -6" />
+                </svg>
+            </button>
+        </div>
+
+
+        <h4 id="title" class="text-center w-100 my-4 p-2 mt-3" data-aos="fade-up">
+            Recent Posts
+        </h4>
+            <ul class="list-group list-group-flush w-100"  data-aos="fade-up">
+                <li class="list-group-item ">An item</li>
+                <li class="list-group-item ">A second item</li>
+                <li class="list-group-item ">A third item</li>
+                <li class="list-group-item ">A fourth item</li>
+                <li class="list-group-item ">And a fifth one</li>
+            </ul>
+        </div>
+
+    </div>
+
+  </div>
 </div>
